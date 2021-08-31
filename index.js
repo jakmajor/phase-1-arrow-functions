4ea781fde5e6a980032e7ea5531fd668f260bf40
Jak Major <hellojakkaiser@gmail.com>
2021-08-31 08:50:56 -0700

    commit tree d042a3910386a183b93da7db3cf24d1fcb485ce2

---
+++ index.js
@@ -0,0 +1,20 @@
+/* const add = (parameter1, parameter2) => parameter1 + parameter2;
+
+//if only one parameter 
+const twoAdder = x => x + 2;
+
+const sum = (parameter1, parameter2) => {
+    console.log(`Adding ${parameter1}`);
+    console.log(`Adding ${parameter2}`);
+    return parameter1 + parameter2;
+} */
+
+const divide = function(a = 2000, b=100) {
+    return a / b;
+}
+
+const square = x => x * x; 
+
+const add = (parameter1, parameter2) => {
+    return parameter1 + parameter2;
+}
